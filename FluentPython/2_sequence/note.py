c97db887157a3db0bf3ca2cf8c8c1d7fc6e5de80
SELO77 <rochan87@gmail.com>
2016-12-26 22:41:32 +0900

    (FluentPython) Sequence bisect

---
+++ FluentPython/2_sequence/note.py
@@ -0,0 +1,18 @@
+from collections.abc import Iterable
+from collections import namedtuple
+
+
+list()
+ord()
+divmod(20, 8)
+namedtuple('City', 'id name coordinate')
+
+
+import dis
+dis.dis(print("ssss"))
+
+
+import bisect
+
+bisect.bisect()
+bisect.insort()
